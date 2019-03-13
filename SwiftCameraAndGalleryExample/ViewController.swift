2caa89117c16264ba0ffcfcd45e8e2f7cc1b3e6b
Jagmeet Kaur <jagmeetkaur@jagmeets-macbook-air.local>
2019-03-13 19:41:09 -0400

    Accessing pictures from gallery

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var imgPhoto: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnCamera(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+           myPickerController.delegate = self;
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true, completion: nil)
+    }
+        func btnGallery(_ sender: UIBarButtonItem) {
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                let myPickerController = UIImagePickerController()
+                myPickerController.delegate = self;
+                myPickerController.sourceType = .photoLibrary
+                self.present(myPickerController, animated: true, completion: nil)
+            }
+    }
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                self.imgPhoto.image = image
+            }else{
+                print("Something went wrong")
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+        
 }
-
+}
